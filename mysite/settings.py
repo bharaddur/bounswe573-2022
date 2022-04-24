b296b72fb56c7e274bde4d8b161ee237eedd67a9
bharaddur <mcangeyik@gmail.com>
2022-04-25 00:51:07 +0300

    admin panel is created, courses model added

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'courses.apps.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
